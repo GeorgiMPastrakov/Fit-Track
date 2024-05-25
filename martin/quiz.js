c3c5e3694a33765b004fa08613af4d915ff37532
MartinMirchev344 <martin.m.mirchev.2023@elsys-bg.org>
2024-05-26 01:56:11 +0300

    Did the scale for the quiz

--- martin/quiz.js
+++ martin/quiz.js
@@ -14,23 +14,6 @@ function calculateScore()
     scoreSpan.textContent = score;
     resultDiv.style.display = 'block';
 
-    let message = '';
-    if (score >= 40) 
-    {
-        message = 'Excellent! You have a very healthy lifestyle.';
-    } 
-    else if (score >= 30) 
-    {
-        message = 'Good job! Your lifestyle is quite healthy.';
-    } 
-    else if (score >= 20) 
-    {
-        message = 'Not bad, but there is room for improvement.';
-    }
-    else 
-    {
-        message = 'You might want to consider making some healthier choices.';
-    }
 }
 
 function openNav() {
